6faf2fade938f7f914f3e75cf483c679ab586c77
discimus <newillianpereira@gmail.com>
2025-11-27 15:33:56 -0300

    First commit

---
+++ TestWithAzureDockerInstance/Program.cs
@@ -0,0 +1,33 @@
+namespace TestWithAzureDockerInstance;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddOpenApi();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapGet("/", () => "Hello World!");
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
